22906d065248bbfaf5427258c1b050f313c346ca
Matisk123 <matis.kohn@edu.esiee-it.fr>
2025-06-12 15:14:37 +0200

    Sauvegarde 1

--- sql/database.php
+++ sql/database.php
@@ -1,16 +1,22 @@
 <?php
+
+// Database avec Mamp
+
 function db_connect() {
     try {
         $host = "localhost";
-        $dbname ="coding_faq"; // Nom de votre base de données
-        $user = "root"; // Nom d'utilisateur par défaut pour XAMPP
-        $password = ""; // Mot de passe vide par défaut pour XAMPP
+        $dbname = "coding_faq";
+        $user = "root";
+        $password = "root"; // Mot de passe par défaut avec MAMP
 
         $db = new PDO(
             "mysql:host=$host;dbname=$dbname;charset=utf8",
             $user,
             $password,
-            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::MYSQL_ATTR_DIRECT_QUERY => true)
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::MYSQL_ATTR_DIRECT_QUERY => true
+            )
         );
 
         return $db;
@@ -18,5 +24,6 @@ function db_connect() {
         die('Erreur : ' . $e->getMessage());
     }
 }
-$db = db_connect() ;
+
+$db = db_connect();
 ?>
